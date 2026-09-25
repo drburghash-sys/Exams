ffaf5d263d322e13e18f3f09ffa2b5b6fcfe215c
drburghash-sys <drburghash@gmail.com>
2026-09-25 23:55:43 +0300

    Refresh cache for Grade 5 decimal focus fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='adaptive-v10-2-g5math-20260925';
+const CACHE='adaptive-v10-3-g5-decimal-focus-20260925';
 const FILES=['./','./index.html','./app.js','./core_bank.js','./third_bank.js','./fifth_bank.js','./scope_fix_bank.js','./curriculum_catalog.js','./curriculum_catalog.json','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
